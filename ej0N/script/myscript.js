712fd70672c464f0a733eff9889a2438b86bd707
xartrex <100383407@alumnos.uc3m.es>
2019-10-17 12:52:22 +0200

    yey

---
+++ ej0N/script/myscript.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+ // When the user clicks on <div>, open the popup
+ function myFunction() {
+   var popup = document.getElementById("myPopup");
+   popup.classList.toggle("show");
+ }
+
+});
