a84ea7fb7a319785964139f0c8990832fcd35e63
xartrex <100383407@alumnos.uc3m.es>
2019-10-19 18:51:53 +0200

    starting with jquery

--- ej0N/script/myscript.js
+++ ej0N/script/myscript.js
@@ -1,9 +1,29 @@
 $(document).ready(function(){
 
  // When the user clicks on <div>, open the popup
- function myFunction() {
-   var popup = document.getElementById("myPopup");
-   popup.classList.toggle("show");
- }
+ $('.cruz').on('click', function(){
+      if(confirm('¿Seguro que quieres borar la caja?'))
+      this.parentElement.style.display = 'none';
+      else;
+    });
+
+$('.like').on('click', function(){
+  $(this).children("div").children("img").attr("src","images/corazon.jpg");
+
+  /* Esto es para poder añadir la posibilidad de revertir el like pero no funciona
+
+  var x = $(this).children("div").children("img").attr(src);
+  if(x == "images/like.jpg")
+    $(this).children("div").children("img").attr("src","images/corazon.jpg");
+  else
+  $(this).children("div").children("img").attr("src","images/like.jpg");*/
+  });
+
+  $('.compartir').on('click', function(){
+    alert("hey");
+    alert(this.parentElement("div").sibling(".imagen").children("h2"));
+  });
+  
+
 
 });
