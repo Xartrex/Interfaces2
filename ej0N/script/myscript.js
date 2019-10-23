8ef20f59ef0e5a38b0058b0b687bb315c0cf9ec4
xartrex <100383407@alumnos.uc3m.es>
2019-10-24 01:00:23 +0200

    union

--- ej0N/script/myscript.js
+++ ej0N/script/myscript.js
@@ -48,6 +48,23 @@ $(document).ready(function(){
     $('.modal-contenido1').fadeOut("slow");
     $('.modal-contenido2').fadeOut("slow");
     $('.pops2').css('visibility', 'hidden');
+    $('.dropdown-comment').css('display', 'none');
+
+  });
+
+  $('.ajustes').on('click', function(){
+    $(this).children(".dropdown-comment").css('display', 'block');
+  });
+
+  $('.cl').on('click', function(){
+   /* $('.dropdown-comment').css('visibility', 'hidden');*/
+    $('.dropdown-comment').fadeOut("slow");
+  });
+
+  $('#arch').on('click', function(){
+    if(confirm('¿Seguro que quieres borrar la caja?'))
+          this.parentElement.parentElement.parentElement.parentElement.style.display = 'none';
+          else;
   });
 
 
@@ -55,93 +72,120 @@ $(document).ready(function(){
     $('.modal_oscuro').fadeIn("slow");
   });
 
-  
-  $('body').on('click', '#guardar', function(){
+ 
 
-    if(localStorage.getItem('email') != $('#email').val()){
 
-      localStorage.setItem('user', $('#user').val());
-      localStorage.setItem('password', $('#password').val());
-      localStorage.setItem('name', $('#name').val());
-      localStorage.setItem('surname', $('#surname').val());
-      localStorage.setItem('email', $('#email').val());
-      localStorage.setItem('date', $('#date').val());
-      localStorage.setItem('interest', $('#interest').val());
-      localStorage.setItem('language', $('#language').val());
-      localStorage.setItem('uso', $('#uso').val());
+/*COOKIES*/ 
 
-    }else{
-      alert('Ya existe una cuenta con este correo electrónico');
-    }
-  });
+$('body').on('click', '#guardar', function(){
 
-  $('.body').on('click', '#login', function(){
+  if(localStorage.getItem('email') != $('#email').val()){
 
-    if(localStorage.getItem('email') == $('#email').val() && localStorage.getItem('password') == $('#password').val()){
-      url = "./index.html";
-      window.location(url);
+    localStorage.setItem('user', $('#user').val());
 
-    }else{
-      alert('El correo o contraseña introducidos son incorrectos');
-    }
-  });
+    localStorage.setItem('password', $('#password').val());
 
-    /*console.log("hola");
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = "formulario" + "=" + document.getElementById("#user").value + "&"+ document.getElementById("#password").value+ "&"+ document.getElementById("#name").value+"&"+ document.getElementById("#surname").value+"&"+ document.getElementById("#email").value+"&"+ document.getElementById("#date").value+"&"+ document.getElementById("#interest").value+"&"+ document.getElementById("#language").value+"&"+ document.getElementById("#uso").value + ";" +  expires + ";path=/";
-  }*/
+    localStorage.setItem('name', $('#name').val());
 
-  function getCookie(posicion) {
-    var formulario = "formulario" + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(formulario) == 0) {
-        return getsubString(c.substring(formulario.length, c.length), posicion);
-      }
-    }
-    return "";
-  }
-  //se divide el formulario dentro de la cookie
-  function gesubSubstring(formulario, posicion){
-    var ca = formulario.split('&');
-      return ca[posicion];
-  }
+    localStorage.setItem('surname', $('#surname').val());
 
-  function checkCookie() {
-    var user = getCookie("username");
-    if (user != "") {
-      alert("Bienvenido de nuevo" + user);
-    } else {
-      user = prompt("Please enter your name:", "");
-      if (user != "" && user != null) {
-        setCookie("username", user, 365);
-      }
-    }
-  }
+    localStorage.setItem('email', $('#email').val());
 
-  function checkInicioSesion(){
+    localStorage.setItem('date', $('#date').val());
 
-    var email = getCookie(4);//posicion del array de la cookie en la que se encuentra el correo electronico
-    var password = getCookie(1);//posicion del array de la cookie en la que se encuentra la contraseña
+    localStorage.setItem('interest', $('#interest').val());
 
-    if(email != document.getElementById("#correoInicioSesion").value) {
-      alert("El correo introducido es incorrecto");
-    }
-    if(password != document.getElementById("contraseñaInicioSesion").value) {
-      alert("El correo introducido es incorrecto");
-    }else{
-      document.location.href = "./index.html";
-    }
+    localStorage.setItem('language', $('#language').val());
 
+    localStorage.setItem('uso', $('#uso').val());
+
+    jump();
+
+  }else{
+
+    alert('Ya existe una cuenta con este correo electrónico');
 
   }
 
+});
+
+$('.body').on('click', '#login', function(){
+
+
+  if(localStorage.getItem('email') == $('#email').val() && localStorage.getItem('password') == $('#password').val()){
+
+    jump();
+    var url = "./index.html";
+    $(location).attr('href', url);
+
+
+  }else{
+
+    alert('El correo o contraseña introducidos son incorrectos');
+  }
+
+});
+
+function jump(){
+      window.open('index.html', '_self');
+    }
+
+/*  console.log("hola");
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = "formulario" + "=" + document.getElementById("#user").value + "&"+ document.getElementById("#password").value+ "&"+ document.getElementById("#name").value+"&"+ document.getElementById("#surname").value+"&"+ document.getElementById("#email").value+"&"+ document.getElementById("#date").value+"&"+ document.getElementById("#interest").value+"&"+ document.getElementById("#language").value+"&"+ document.getElementById("#uso").value + ";" +  expires + ";path=/";
+}
+
+function getCookie(posicion) {
+  var formulario = "formulario" + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(formulario) == 0) {
+      return getsubString(c.substring(formulario.length, c.length), posicion);
+    }
+  }
+  return "";
+}
+//se divide el formulario dentro de la cookie
+function gesubSubstring(formulario, posicion){
+  var ca = formulario.split('&');
+    return ca[posicion];
+}
+
+function checkCookie() {
+  var user = getCookie("username");
+  if (user != "") {
+    alert("Bienvenido de nuevo" + user);
+  } else {
+    user = prompt("Please enter your name:", "");
+    if (user != "" && user != null) {
+      setCookie("username", user, 365);
+    }
+  }
+}
+
+function checkInicioSesion(){
+
+  var email = getCookie(4);//posicion del array de la cookie en la que se encuentra el correo electronico
+  var password = getCookie(1);//posicion del array de la cookie en la que se encuentra la contraseña
+
+  if(email != document.getElementById("#correoInicioSesion").value) {
+    alert("El correo introducido es incorrecto");
+  }
+  if(password != document.getElementById("contraseñaInicioSesion").value) {
+    alert("El correo introducido es incorrecto");
+  }else{
+    document.location.href = "./index.html";
+  }
+
+
+}
+*/
 
 
 
